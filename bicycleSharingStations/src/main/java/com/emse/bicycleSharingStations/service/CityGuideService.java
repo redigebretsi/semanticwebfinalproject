34d2e6cc78ad39154fc41c169bde3597e52e5632
Your Name <you@example.com>
2020-12-15 22:48:23 +0100

    query for hospital

--- bicycleSharingStations/src/main/java/com/emse/bicycleSharingStations/service/CityGuideService.java
+++ bicycleSharingStations/src/main/java/com/emse/bicycleSharingStations/service/CityGuideService.java
@@ -40,7 +40,7 @@ public class CityGuideService {
 //                "  FILTER (?cityName = \""+cname+ "\")\n" +
 //                "  }ORDER BY ?stationName   ";
 
-        String query = "PREFIX onto: <http://www.semanticweb.org/emse/ontologies/2019/11/bicycle_stations.owl#>\n" +
+        String query = "PREFIX onto: <http://www.semanticweb.org/emse/ontologies/2020/11/city.owl#>\n" +
                 "PREFIX geo: <https://www.w3.org/2003/01/geo/wgs84_pos#>\n" +
                 "PREFIX schema: <http://schema.org/>\n" +
                 "PREFIX ont: <http://purl.org/net/ns/ontology-annot#>\n" +
@@ -101,33 +101,22 @@ public class CityGuideService {
         List<Hospital> hospitalList = new ArrayList<Hospital>();
 
 
-        String query = "PREFIX onto: <http://www.semanticweb.org/emse/ontologies/2019/11/bicycle_stations.owl#>\n" +
-                "PREFIX geo: <https://www.w3.org/2003/01/geo/wgs84_pos#>\n" +
-                "PREFIX schema: <http://schema.org/>\n" +
-                "PREFIX ont: <http://purl.org/net/ns/ontology-annot#>\n" +
-                "prefix xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
-                "\n" +
-                "SELECT ?stationName ?city ?station ?capacity ?lat ?lon ?updatedDateTime ?availableBikes\n" +
-                "WHERE {\n" +
-                "  ?city onto:cityName ?cityName .\n" +
-                "  ?city onto:hasStation ?station .\n" +
-                "  ?station onto:stationName ?stationName .\n" +
-                "  ?station onto:capacity ?capacity .\n" +
-                "  ?station geo:lat ?lat .\n" +
-                "  ?station geo:long ?lon .\n" +
-                "  ?station onto:hasAvailability ?availability .\n" +
-                "  ?availability onto:updatedDatetime ?updatedDateTime .\n" +
-                "  ?availability onto:availableBikes ?availableBikes .  \n" +
-                "  {SELECT ?station (MAX(?dt)  AS ?updatedDateTime)\n" +
-                "    WHERE {\n" +
-                "      ?city onto:cityName ?cityName .\n" +
-                "      ?city onto:hasStation ?station .\n" +
-                "      ?station onto:hasAvailability ?ava .\n" +
-                "      ?ava onto:updatedDatetime ?dt .\n" +
-                "      ?ava onto:availableBikes ?availableBikes .\n" +
-                "    } GROUP BY ?station\n" +
-                "  }\n" +
-                "}ORDER BY ?stationName   ";
+        String query = "PREFIX http: <http://www.w3.org/2011/http#>\r\n" + 
+        		"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\r\n" + 
+        		"PREFIX onto: <http://www.semanticweb.org/emse/ontologies/2020/11/city.owl#>\r\n" + 
+        		"PREFIX schema: <http://schema.org/>\r\n" + 
+        		"PREFIX ont: <http://purl.org/net/ns/ontology-annot#>\r\n" + 
+        		"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\r\n" + 
+        		"\r\n" + 
+        		"SELECT ?category  ?lat ?lon ?address ?telephone ?hospital\r\n" + 
+        		"WHERE {\r\n" + 
+        		"  ?hospital a onto:Hospital.\r\n" + 
+        		"  ?hospital onto:catagorie ?category.\r\n" + 
+        		"  ?hospital onto:hasLatitude ?lat .\r\n" + 
+        		"  ?hospital onto:hasLongitude ?lon .\r\n" + 
+        		"  ?hospital onto:tel_number ?telephone .\r\n" + 
+        		"  ?hospital onto:adresse ?address.\r\n" + 
+        		"}";
 
 
         Query qu = QueryFactory.create(query);
@@ -136,21 +125,21 @@ public class CityGuideService {
 
         while (results.hasNext()) {
             QuerySolution qs = results.next();
-            Resource stationIRI = (Resource) qs.get("station");
-            RDFNode name = qs.get("stationName");
+            Resource hospitalIRI = (Resource) qs.get("hospital");
+            RDFNode category = qs.get("category");
             RDFNode lat = qs.get("lat");
             RDFNode lon = qs.get("lon");
-            RDFNode capacity = qs.get("capacity");
-            RDFNode availableBikes = qs.get("availableBikes");
-            RDFNode updatedDateTime = qs.get("updatedDateTime");
+            RDFNode address = qs.get("address");
+            RDFNode telephone = qs.get("telephone");
+//            RDFNode updatedDateTime = qs.get("updatedDateTime");
 
-            String lIRI = stationIRI.getURI();
+            String lIRI = hospitalIRI.getURI();
             System.out.println(lIRI);
-            String adresse = name.asLiteral().getString();
+            String adresse = address.asLiteral().getString();
             double latitude = lat.asLiteral().getDouble();
             double longitude = lon.asLiteral().getDouble();
-            String tel_number = capacity.asLiteral().getString();
-            String catagorie = availableBikes.asLiteral().getString();
+            String tel_number = telephone.asLiteral().getString();
+            String catagorie = category.asLiteral().getString();
             
             hospitalList.add(new Hospital( adresse,  tel_number,  catagorie,  latitude,  longitude));
         }
@@ -158,8 +147,12 @@ public class CityGuideService {
     }
    
     public List <BTMStations> findBTMmodel(String type) {
+    	 List<BTMStations> btmList = new ArrayList<BTMStations>();
+    	if(type=="BUSSTATIONS") {
+    		
+    	}
 
-        List<BTMStations> btmList = new ArrayList<BTMStations>();
+       
 
 
         String query = "PREFIX onto: <http://www.semanticweb.org/emse/ontologies/2019/11/bicycle_stations.owl#>\n" +
